a014603a3d52038fb641e44690a287a3b6eddeb0
Luis Naranjo <luisnaranjo733@gmail.com>
2015-12-01 06:39:38 -0800

    created leaflet map with markers and circle
    Loading markers from JSON data in data/marker_coordinates.json
    Marker coordinates all in nearby neighborhoods
    Using one coordinate and radius (miles) as parameters for a circle
    This circle represents a driver's desired rider pick up area.
    If you change the value of the radius via the input form,
    the size of the circle changes with it.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,35 @@
 'use strict';
 
+const miles_to_meters = 1609.34;
+
+var map = L.map('map').setView([47.6550, -122.3080], 10);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.outdoors',
+    accessToken: 'pk.eyJ1IjoibHVpc25hcmFuam83MzMiLCJhIjoiY2lmeDVra3Q1M3A0Z3U2a3N3d2JzNXFicCJ9.nLZGt1FxRVUxOUL-_1wrIg'
+}).addTo(map);
+
 var carpoolApp = angular.module('carpoolApp', []);
 
 carpoolApp.controller('carpoolCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.greeting = "Hello, World!";
+    $scope.latitude = 47.752629;
+    $scope.longitude = -122.285041;
+    var center = L.circleMarker([$scope.latitude, $scope.longitude]);
+    $scope.radius = 1;
+
+    var circle = L.circle([$scope.latitude, $scope.longitude], $scope.radius * miles_to_meters).addTo(map);
+  
+    
+    $http.get('data/marker_coordinates.json').then(function(response) {
+        response.data.forEach(function(coordinate) {
+            var marker = L.marker([coordinate.lat, coordinate.lng]).addTo(map);
+        })
+    })
+
+    $scope.update_radius = function() {
+        circle.setRadius($scope.radius * miles_to_meters);
+    }
 
 }]);
