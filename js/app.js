2fba08b51f195a219a4facd92b452131bac9d8fc
William Menten-Weil <wtmenten@gmail.com>
2015-12-01 23:14:07 -0800

    finished login form with validation

--- js/app.js
+++ js/app.js
@@ -1,9 +1,47 @@
 'use strict';
-var carpoolApp = angular.module('carpoolApp', ['ui.router', 'firebase']);
+var carpoolApp = angular.module('carpoolApp', ['ui.router', 'ui.validate', 'firebase']);
 
 carpoolApp.controller('carpoolCtrl', function($scope, $http, $firebaseObject) {
 
+    $scope.validUwEmail = function(value) {
+        if (angular.isUndefined(value)) {
+            return false;
+        }
+        var host  = value.split("@").pop();
+        if (host == "uw.edu" || host == "u.washington.edu") {
+          // it is valid
+          return true;
+        }
+        return false;
+    };
+
     var ref = new Firebase("https://uwcarpool.firebaseio.com/data");
+
+    var login = function() {
+        ref.authWithPassword({
+          email    : $scope.email,
+          password : $scope.password
+        }, function(error, authData) {
+          if (error) {
+            console.log("Login Failed!", error);
+          } else {
+            console.log("Authenticated successfully with payload:", authData);
+          }
+        });
+    }
+
+    var signup = function() {
+        ref.createUser({
+          email    : $scope.email,
+          password : $scope.password
+        }, function(error, userData) {
+          if (error) {
+            console.log("Error creating user:", error);
+          } else {
+            console.log("Successfully created user account with uid:", userData.uid);
+          }
+        });
+    }
     // download the data into a local object
     var syncObject = $firebaseObject(ref);
     // synchronize the object with a three-way data binding
@@ -30,5 +68,20 @@ carpoolApp.config(function($stateProvider, $urlRouterProvider, $locationProvider
     .state('Home', {
         url: "/",
         templateUrl: "partial/login.html",
+        controller: function($scope, $firebaseObject) {
+            var ref = new Firebase("https://uwcarpool.firebaseio.com/data");
+            var login = function() {
+                ref.authWithPassword({
+                  email    : $scope.email,
+                  password : $scope.password
+                }, function(error, authData) {
+                  if (error) {
+                    console.log("Login Failed!", error);
+                  } else {
+                    console.log("Authenticated successfully with payload:", authData);
+                  }
+                });
+            }
+        }
     })
 });
