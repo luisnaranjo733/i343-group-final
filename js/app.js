7b8ed15fe3fc75ca1526d207e073f13003a22529
William Menten-Weil <wtmenten@gmail.com>
2015-12-01 16:23:41 -0800

    added dependent files for angular, bootstrap and firebase, updates the app.js and index.html pages with these additions.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,34 @@
 'use strict';
+var carpoolApp = angular.module('carpoolApp', ['ui.router', 'firebase']);
 
+carpoolApp.controller('carpoolCtrl', function($scope, $http, $firebaseObject) {
 
-var carpoolApp = angular.module('carpoolApp', []);
+    var ref = new Firebase("https://uwcarpool.firebaseio.com/data");
+    // download the data into a local object
+    var syncObject = $firebaseObject(ref);
+    // synchronize the object with a three-way data binding
+    // click on `index.html` above to see it used in the DOM!
+    syncObject.$bindTo($scope, "firebaseData");
 
-carpoolApp.controller('carpoolCtrl', ['$scope', '$http',  function($scope, $http) {
-  
+});
 
-}]);
+carpoolApp.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+    //
+    // For any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise("/");
+    // $locationProvider.html5Mode(true);
+
+    //
+    // Now set up the states
+    $stateProvider
+    .state('Template', {
+        url: "/route/to/:id",
+        templateUrl: function (stateParams){
+            return 'partial/template/by/' + stateParams.step + '.html';
+        }
+    })
+    .state('Home', {
+        url: "/",
+        templateUrl: "partial/login.html",
+    })
+});
