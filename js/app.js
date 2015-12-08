bc7beec267f53c4be634dadd7674eded7703cb95
Luis Naranjo <luisnaranjo733@gmail.com>
2015-12-08 11:52:30 -0800

    tabs are working with driver code inside!

--- js/app.js
+++ js/app.js
@@ -264,8 +264,122 @@ carpoolApp.controller('carpoolCtrl', function($scope, $http, $firebaseObject, au
     }
 
 }])
-.controller('driversController', ['$scope', '$state', function($scope, $state) {
+.controller('driversController', ['$scope', '$firebaseObject', 'FIREBASE_URI', 'userService', '$http', '$firebaseAuth', '$state', function($scope, $firebaseObject, FIREBASE_URI, userService, $http, $firebaseAuth, $state){
 
+
+
+    // var slider = new Slider('#ex1', {
+    //     formatter: function(value) {
+    //         return 'Pick up zone radius: ' + value;
+    //     },
+    //     tooltip: 'always'
+    // });
+    // slider.disable();
+
+    var ref = new Firebase(FIREBASE_URI);
+    $scope.authObj = $firebaseAuth(ref);
+
+    var map = L.map('map').setView([47.745169, -122.288939], 11);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.outdoors',
+        accessToken: 'pk.eyJ1IjoibHVpc25hcmFuam83MzMiLCJhIjoiY2lmeDVra3Q1M3A0Z3U2a3N3d2JzNXFicCJ9.nLZGt1FxRVUxOUL-_1wrIg'
+    }).addTo(map);
+
+    var rider_markers = L.layerGroup([]);
+
+    // loops over each marker in rider_markers layerGroup
+    // highlights it if its is within the circle
+    // dims it if is not
+    // called from  $scope.update_radius()
+    // which is called from the html view
+    var filter_markers = function(circle) {
+        rider_markers.eachLayer(function(layer) {
+            // Lat, long of current point
+            var layer_lat_long = layer.getLatLng();
+            var circle_lat_long = circle.getLatLng();
+            var distance_from_layer_circle = layer_lat_long.distanceTo(circle_lat_long);
+            //console.log(distance_from_layer_circle / meters_miles_const);
+            var miles_away = distance_from_layer_circle / meters_miles_const;
+
+            if (miles_away < circle.getRadius() / meters_miles_const) {
+                layer.setOpacity(1);
+            } else {
+                layer.setOpacity(0.6);
+            }
+        })    
+    }
+
+
+    var authData = $scope.authObj.$getAuth();
+    if (authData) {
+        var user = userService.getUser(authData.uid).$loaded(function(user) {
+
+
+            // radius in miles
+            // attach to angular model
+            // and html input element
+            
+            if (user.pickUpRadius) {
+                $scope.pickUpRadius = user.pickUpRadius;
+            } else {
+                user.pickUpRadius = 2;
+                user.$save();
+                $scope.pickUpRadius = user.pickUpRadius;
+            }
+
+            // slider.setValue($scope.pickUpRadius)
+            // slider.enable();
+
+            // slider.on('slide', function(value) {
+            //     $scope.pickUpRadius = value;
+            //     $scope.update_radius();
+            // });
+
+
+            var circle = L.circle([user.lat, user.lng], $scope.pickUpRadius * meters_miles_const).addTo(map);
+
+            $http.get('data/marker_coordinates.json').then(function(response) {
+                response.data.forEach(function(coordinate) {
+                    var marker = L.marker([coordinate.lat, coordinate.lng], {opacity: 0.6});
+                    marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+                    marker.addTo(rider_markers);
+                });
+                rider_markers.addTo(map)
+                filter_markers(circle)
+                
+            });
+
+
+            $scope.update_radius = function() {
+                circle.setRadius($scope.pickUpRadius * meters_miles_const);
+                filter_markers(circle);
+                user.pickUpRadius = $scope.pickUpRadius;
+                user.$save();
+            }
+
+            // Create an event listener while mouse is being held down on the circle
+            circle.on('mousedown', function(mousedown_event) {
+                map.on('mousemove', function(mousemove_event) {
+                    circle.setLatLng(mousemove_event.latlng);
+
+         
+                    // highlights markers within radius of circle
+                    // dims all others
+                    filter_markers(circle);
+                })
+            });
+
+            // remove event listener when mouse is removed
+            map.on('mouseup', function(e) {
+                map.removeEventListener('mousemove');
+                user.lat = e.latlng.lat;
+                user.lng = e.latlng.lng;
+                user.$save();
+            })
+        });
+    }
     
 }])
 .controller('ridersController', ['$scope', '$state', function($scope, $state) {
