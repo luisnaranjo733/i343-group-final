25094469deb90f7ca2bfe32c1c0ec33be9aef1b0
William Menten-Weil <wtmenten@gmail.com>
2015-12-10 00:10:41 -0800

    fix dereferencing currentUser

--- js/app.js
+++ js/app.js
@@ -121,7 +121,7 @@ carpoolApp.controller('carpoolCtrl', function($rootScope, $scope, $http, $fireba
 
     $scope.logout = function() {
         authService.logout();
-        $rootScope.currentUser = null;
+        delete $rootScope.currentUser;
         $state.go("Login");
     }
 
